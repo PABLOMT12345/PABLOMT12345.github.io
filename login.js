8c3004566bce3e50ed9dfe0bc03b546285b27715
PABLOMT12345 <157745404+PABLOMT12345@users.noreply.github.com>
2024-01-25 20:42:44 -0600

    Update login.js

--- login.js
+++ login.js
@@ -12,25 +12,20 @@ function validateForm() {
     const passwordInput = document.getElementById('password');
     const errorMessage = document.getElementById('error-message');
 
-    // Expresión regular para validar el formato del correo electrónico
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    // Validar email
     if (!emailRegex.test(emailInput.value)) {
         errorMessage.innerText = 'Please enter a valid email address.';
         return;
     }
 
-    // Validar longitud mínima de la contraseña
     if (passwordInput.value.length < 8) {
         errorMessage.innerText = 'Password must be at least 8 characters long.';
         return;
     }
 
-    // Si la validación es exitosa, limpiar el mensaje de error
     errorMessage.innerText = '';
 
-    // Aquí puedes realizar la lógica de inicio de sesión si es necesario
-    // Por ahora, simplemente mostraremos un mensaje de éxito en la consola
+
     console.log('Login successful!');
 }
